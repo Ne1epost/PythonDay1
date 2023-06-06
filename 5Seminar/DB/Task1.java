66b1c302625e35ea8660fc891df6a72f756351d6
Ne1epost <119726311+Ne1epost@users.noreply.github.com>
2023-06-06 21:15:28 +0300

    HomeTask DB

---
+++ 5Seminar/DB/Task1.java
@@ -0,0 +1,11 @@
+package org.example.lesson1.DB;
+
+public class Task1 {
+    public static void sort (int[] array){
+        for (i = array.length/2 -1 ; i >= 0; i--)
+            heapify(array,array.length,i);
+        for (int i = array; i < ; i++) {
+
+        }
+    }
+}
